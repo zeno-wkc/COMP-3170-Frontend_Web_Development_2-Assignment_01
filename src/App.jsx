7aeeb22f3a57c687eee989f6ea1f9f67fdd73ef4
Zeno <zenowkc@gmail.com>
2024-09-13 17:48:08 -0700

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,12 @@ function App() {
                   memory and ideally some kind of long-term storage. An input device as well
                   as some form of output device is recommended.
                 </p>
+                <summary>System Requirements</summary>
+                <p>
+                  Requires a computer running an operating system. The computer must have some
+                  memory and ideally some kind of long-term storage. An input device as well
+                  as some form of output device is recommended.
+                </p>
               </details>
             </div>
           </div>
