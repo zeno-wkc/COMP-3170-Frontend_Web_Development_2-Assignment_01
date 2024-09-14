677ad4bbe3602eaffdcba17cd17f1552e8ea6896
Zeno <zenowkc@gmail.com>
2024-09-13 17:54:23 -0700

    image path 13

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,8 @@ function App() {
                   memory and ideally some kind of long-term storage. An input device as well
                   as some form of output device is recommended.
                 </p>
+              </details>
+              <details>
                 <summary>System Requirements</summary>
                 <p>
                   Requires a computer running an operating system. The computer must have some
