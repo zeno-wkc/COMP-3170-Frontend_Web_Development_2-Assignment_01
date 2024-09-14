9ec7366f8ee7dfc2d6d6b10fea6d54da0d643386
Zeno <zenowkc@gmail.com>
2024-09-13 17:31:33 -0700

    image path 05

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <main className="app-content">
           <div className="content-container">
             <div className='left-content-container'>
-              <img src={require('./public/justin-bieber.gif')} alt='Justin Bieber' />
+              <img src={require('/public/justin-bieber.gif')} alt='Justin Bieber' />
             </div>
             <div className='right-content-container'>
               <h2>Justin Bieber's Albums</h2>
